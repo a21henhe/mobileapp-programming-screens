12d2861bb36ae55fe241ccfa0ad41e27ee3ac41b
Henric Hemmander <a21henhe@student.his.se>
2022-04-13 14:21:17 +0200

    main activity now sends string to second activitys textView

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    Intent intent;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-
+        intent = getIntent();
+        textView = findViewById(R.id.secondActivityTextView);
+        textView.setText(intent.getStringExtra("NAME"));
 
     }
 
